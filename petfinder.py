2061c7619fad31b91c707363993d85fb5572a312
Ethan Cloin <psnethan@gmail.com>
2025-04-12 14:31:36 -0400

    Add get_dogs and saved first json data batch

--- petfinder.py
+++ petfinder.py
@@ -2,20 +2,24 @@ import httpx
 from dotenv import load_dotenv
 import os
 import json
+from pathlib import Path
+
+ACCESS_TOKEN = "blank"
+API_KEY = ""
+SECRET = ""
 
 
 def main():
     load_dotenv()
+    global API_KEY, SECRET
     API_KEY = os.getenv("PETFINDER_API_KEY")
     SECRET = os.getenv("PETFINDER_SECRET")
 
-    get_petfinder_auth(API_KEY, SECRET)
+    # get_petfinder_auth(API_KEY, SECRET)
+    get_dogs()
 
 
 def get_petfinder_auth(api_key, secret):
-    """
-    curl -d "grant_type=client_credentials&client_id={CLIENT-ID}&client_secret={CLIENT-SECRET}" https://api.petfinder.com/v2/oauth2/token
-    """
     payload = {
         "grant_type": "client_credentials",
         "client_id": api_key,
@@ -26,7 +30,50 @@ def get_petfinder_auth(api_key, secret):
     response = httpx.post(url, data=payload)
     if response.is_success:
         with open("token.json", "w") as token_file:
-            json.dump(response.json(), token_file)
+            token: dict = response.json()
+            print(type(token))
+            json.dump(token, token_file)
+            global ACCESS_TOKEN
+            ACCESS_TOKEN = token.get("access_token", "")
+    else:
+        response.raise_for_status()
+
+
+def get_dogs(is_retry=False):
+    params = {
+        "type": "Dog",
+        "status": "adoptable,adopted,found",
+        "location": "Jacksonville, FL",
+        "distance": 500,
+        "sort": "distance",
+        "page": 1,
+        "limit": 100,
+    }
+    url = "https://api.petfinder.com/v2/animals"
+    token_value = "Bearer " + ACCESS_TOKEN
+    headers = {"Authorization": token_value}
+
+    response = httpx.get(url, params=params, headers=headers)
+    if response.is_success:
+        content = response.json()
+        animal_type = params.get("type")
+        # city-state
+        location = params.get("location", "").replace(", ", "-")
+        # {page-num_animals_returned}
+        page_and_count = (
+            str(params.get("page", 1)) + "-" + str(len(content.get("animals", [])))
+        )
+        save_filename = f"{animal_type}_{location}_{page_and_count}.json"
+        save_to_path = Path("data") / save_filename
+
+        with open(save_to_path, "w") as f:
+            json.dump(content, f)
+    elif response.status_code == 401 and not is_retry:
+        print("Auth Error! Refreshing token.")
+        get_petfinder_auth(API_KEY, SECRET)
+        get_dogs(is_retry=True)
+    else:
+        response.raise_for_status()
 
 
 if __name__ == "__main__":
