c56d830ca25031a57bd5ee475d9010fc723272e6
Ethan Cloin <psnethan@gmail.com>
2025-04-29 17:50:11 -0400

    Use Johns detection model

--- app/config.py
+++ app/config.py
@@ -12,3 +12,4 @@ class Config:
     UPLOAD_PATH = APP_BASE_PATH / "static" / "uploads"
     MODEL_PATH = APP_BASE_PATH / "models" / "dog_breed_model"
     DATABASE_FILE = APP_BASE_PATH / "feedback.db"
+    DETECTION_PATH = APP_BASE_PATH / "models" / "dog_detection"
